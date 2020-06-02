cb80d59982928c3d19a948b7780fe28fe81f63b1
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2020-06-02 09:49:01 +0900

    add reactivity to tabs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 // @flow
-import React from 'react';
+import React, {Component} from 'react';
 import './App.scss';
 import Tabs from './components/Tabs'
 import AddressTile from './components/AddressTile'
 //import 'react-bulma-components/dist/react-bulma-components.min.css';
 //import { Button } from 'react-bulma-components';
 
-function App() {
-  return (
-    <div>
-      <h1 className="App-title">설정</h1>
-      <Tabs/>
-      <p className="App-subtitle">등록된 배송지</p>
-      <a className="App-link">+추가</a>
-      <AddressTile/>
-    </div>
-  )
+class App extends Component {
+  render() {
+    const tabs = ['프로필', '계정', '결제수단', '배송지', '알림']
+    return (
+      <div>
+        <h1 className="App-title">설정</h1>
+        <Tabs characterData={tabs}/>
+        <p className="App-subtitle">등록된 배송지</p>
+        <a className="App-link">+추가</a>
+        <AddressTile/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react'
 
 class Tabs extends Component {
   render() {
-    return (
-      <div className="tabs">
+    const {characterData} = this.props;
+    const tabs = characterData.map((tab, index)=>{
+      return (
         <ul>
-          <li className="is-active"><a>프로필</a></li>
-          <li><a>계정</a></li>
-          <li><a>결제수단</a></li>
-          <li><a>배송지</a></li>
-          <li><a>알림</a></li>
+          <li key={index}><a>{tab}</a></li>
         </ul>
-      </div>
-    )
+      )
+    })
+  return <div className="tabs">{tabs}</div>
   }
 }
 
