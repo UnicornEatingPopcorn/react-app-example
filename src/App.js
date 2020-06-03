88030aff655fdfba1a750bdae169ae555e93955e
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2020-06-03 15:52:21 +0900

    create desktop version

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.sass';
 import Tabs from './components/Tabs'
 import AddressTile from './components/AddressTile'
 import AppInfo from './components/AppInfo'
-//import 'react-bulma-components/dist/react-bulma-components.min.css';
-//import { Button } from 'react-bulma-components';
 
 class App extends Component {
   render() {
@@ -13,20 +11,46 @@ class App extends Component {
       <div>
         <h1 className="App_title">설정</h1>
         <Tabs/>
-        <p className="App_subtitle">등록된 배송지</p>
-        <a className="App_link">+추가</a>
         <div id="mobile-version">
-          <AddressTile/>
-          <div className="App_button">더보기</div>
-          <AppInfo/>
+          <div className="App_mobile-container">
+            <p className="App_subtitle">등록된 배송지</p>
+            <a className="App_link">+추가</a>
+          </div>
+          <div className="App_mobile-tiles">
+            <AddressTile/>
+            <div className="App_button">더보기</div>
+            <AppInfo/>
+          </div>
         </div>
         <div id="tablet-version">
           <div className="App_tablet-container">
+            <p className="App_subtitle">등록된 배송지</p>
+            <a className="App_link">+추가</a>
+          </div>
+          <div className="App_tablet-tiles">
             <AddressTile/>
             <div className="App_button">더보기</div>
           </div>
           <AppInfo/>
         </div>
+        <div id="desktop-version">
+          <div className="columns">
+            <div className="column is-paddingless is-9">
+              <div className="App_tablet-container">
+                <p className="App_subtitle">등록된 배송지</p>
+                <a className="App_link">+추가</a>
+              </div>
+              <div className="App_tablet-tiles">
+                <AddressTile/>
+                <div className="App_button">더보기</div>
+              </div>
+            </div>
+            <div className="column is-paddingless">
+              <AppInfo/>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
