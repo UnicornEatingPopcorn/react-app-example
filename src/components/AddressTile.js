adb95184c00f36ce35bc746247a7bbccb3402517
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2020-06-05 03:17:30 +0900

    fix hoverable menu behavior

--- src/components/AddressTile.js
+++ src/components/AddressTile.js
@@ -1,37 +1,27 @@
 import React, { Component } from "react";
 import AppMenu from './AppMenu';
-import Modal from "./Modal";
 
 class AddressTile extends Component {
   constructor(props) {
       super(props);
       this.handleClick = this.handleClick.bind(this);
-      this.handleTagClick = this.handleTagClick.bind(this);
       this.state = {
         isClicked: false,
         //isToggleOn: false,
-        modalOpen: false
       };
   }
 
   handleClick() {
-    this.setState( state=>({
-      isClicked: !state.isClicked,
+    this.setState( ()=>({
+      isClicked: true,
       //isToggleOn: !state.isToggleOn
     }));
-  }
-
-  handleTagClick() {
-    this.setState( state=> ({
-      modalOpen: !state.modalOpen
-    }))
   }
 
   render() {
     const address = this.props.address
     //const toggle = this.state.isToggleOn
     const setDefaultAddress = this.state.isClicked
-    const modalOpenRequest = this.state.modalOpen
 
     return (
       <div>
@@ -39,7 +29,7 @@ class AddressTile extends Component {
           <p className="AddressTile__postnumber">[{address.postnumber}]</p>
           <p className="AddressTile__address">{address.address}</p>
           <AppMenu/>
-          <span className={setDefaultAddress ? "AddressTile__tag" : ""} onClick={this.handleTagClick}>
+          <span className={setDefaultAddress ? "AddressTile__tag" : ""}>
             <p className={setDefaultAddress ? "visible" : "is-hidden"}>기본
             </p>
           </span>
@@ -50,7 +40,6 @@ class AddressTile extends Component {
             </p>
           </span>
         </div>
-        <Modal isOpened={modalOpenRequest}/>
       </div>
     );
 
--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import button from '../button.png'
+import button from '../button.png';
+import Modal from "./Modal";
 
 class AppMenu extends Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
     this.handleMouseHover = this.handleMouseHover.bind(this);
     this.state = {
       isHovering: false,
+      modalOpen: false
     };
   }
 
@@ -20,19 +23,28 @@ class AppMenu extends Component {
     };
   }
 
+  handleClick() {
+    this.setState( state=> ({
+      modalOpen: !state.modalOpen
+    }))
+  }
+
   render() {
+    const modalOpenRequest = this.state.modalOpen
+
     return (
       <div>
-        <div onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
+        <div className="App__container" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
           <img src={button} className="AddressTile__button"></img>
         </div>
         {
           this.state.isHovering &&
           <div className="App__menu">
             <p className="App__menu-selected">기본 배송지 설정</p>
-            <p className="App__menu-option">삭제</p>
+            <p className="App__menu-delete" onClick={this.handleClick}>삭제</p>
           </div>
         }
+        <Modal isOpened={modalOpenRequest}/>
       </div>
     );
   }
