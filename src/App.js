631ebf0b8e7899a5f0862ab1f3353979a207c3f1
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2020-06-05 16:43:55 +0900

    create NewAddressForm component

--- src/App.js
+++ src/App.js
@@ -5,50 +5,63 @@ import './variables.sass';
 import Tabs from './components/Tabs'
 import AddressTiles from './components/AddressTiles'
 import AppInfo from './components/AppInfo'
+import NewAddressForm from './components/NewAddressForm';
 
 class App extends Component {
-   render() {
+  constructor(props){
+    super(props);
+    this.state = {
+       addresses: []
+    }
+  }
+
+  handleSubmit = address => {
+    this.setState({ addresses: [...this.state.addresses, address] })
+  }
+
+  render() {
     return (
-      <div>
-        <h1 className="App__title">설정</h1>
-        <Tabs/>
-        <div id="mobile-version">
-          <div className="App__mobile-container">
-            <p className="App__subtitle">등록된 배송지</p>
-            <a className="App__link">+추가</a>
+    <div>
+      <h1 className="App__title">설정</h1>
+      <Tabs/>
+      <div id="mobile-version">
+        <div className="App__mobile-container">
+          <p className="App__subtitle">등록된 배송지</p>
+          <a className="App__link">+추가</a>
+        </div>
+        <div className="App__mobile-tiles">
+          <AddressTiles/>
+          <AppInfo/>
+        </div>
+      </div>
+      <div id="tablet-version">
+        <div className="App__tablet-container">
+          <p className="App__subtitle">등록된 배송지</p>
+          <a className="App__link">+추가</a>
+        </div>
+        <div className="App__tablet-tiles">
+          <AddressTiles/>
+        </div>
+        <AppInfo/>
+      </div>
+      <div id="desktop-version">
+        <div className="columns">
+          <div className="column is-paddingless is-9">
+            <div className="App__tablet-container">
+              <p className="App__subtitle">등록된 배송지</p>
+              <a className="App__link">+추가</a>
+            </div>
+            <div className="App__tablet-tiles">
+              <AddressTiles/>
+            </div>
           </div>
-          <div className="App__mobile-tiles">
-            <AddressTiles/>
+          <div className="column is-paddingless">
             <AppInfo/>
           </div>
         </div>
-        <div id="tablet-version">
-          <div className="App__tablet-container">
-            <p className="App__subtitle">등록된 배송지</p>
-            <a className="App__link">+추가</a>
-          </div>
-          <div className="App__tablet-tiles">
-            <AddressTiles/>
-          </div>
-          <AppInfo/>
-        </div>
-        <div id="desktop-version">
-          <div className="columns">
-            <div className="column is-paddingless is-9">
-              <div className="App__tablet-container">
-                <p className="App__subtitle">등록된 배송지</p>
-                <a className="App__link">+추가</a>
-              </div>
-              <div className="App__tablet-tiles">
-                <AddressTiles/>
-              </div>
-            </div>
-            <div className="column is-paddingless">
-              <AppInfo/>
-            </div>
-          </div>
-        </div>
       </div>
+      <NewAddressForm handleSubmit={this.handleSubmit}/>
+    </div>
     )
   }
 }
---
+++ src/components/NewAddressForm.js
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+
+class NewAddressForm extends Component {
+  constructor(props) {
+    super(props);
+    this.initialState = {
+      name: "",
+      postalCode: "",
+      address: ""
+    }
+
+    this.state = this.initialState
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.handleSubmit(this.state)
+    this.setState(this.initialState)
+}
+
+  handleInputChange = event => {
+    const { name, value } = event.target
+
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  render() {
+    const {name, postalCode, address} = this.state;
+
+    return (
+      <form className="NewAddressForm" onSubmit={this.handleSubmit}>
+        <div className="NewAddressForm__start">
+          <p className="NewAddressForm__title">배송지 추가</p>
+          <p className="NewAddressForm__close-button">X</p>
+        </div>
+        <div className="control NewAddressForm__name-input">
+          <input className="input" type="text" name="name" value={name} placeholder="받는 사람" onChange={this.handleInputChange}/>
+        </div>
+        <div className="control NewAddressForm__postalCode-input">
+          <input className="input" type="text" name="postalCode" value={postalCode} placeholder="우편번호" onChange={this.handleInputChange}/>
+        </div>
+        <div className="control NewAddressForm__address-input">
+          <input className="input" type="text" name="address" value={address} placeholder="주소" onChange={this.handleInputChange}/>
+        </div>
+        <div className="control">
+          <label className="checkbox">
+            <input type="checkbox"/>
+            기본 배송지로 등록
+          </label>
+        </div>
+        <div className="control">
+          <button className="button NewAddressForm__button is-danger">등록 완료</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default NewAddressForm
